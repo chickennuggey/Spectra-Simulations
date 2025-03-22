9cc030315e3fe582fd4242925d8d7658ff0fa679
briananguyen <109394854+chickennuggey@users.noreply.github.com>
2025-03-21 21:25:51 -0700

    Add files via upload

---
+++ XANES/code/job.sh
@@ -0,0 +1,22 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\csgray\c0;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\fs22 \cf2 \CocoaLigature0 #!/bin/bash -l\
+\
+#$ -cwd\
+#$ -l nodes=1\
+#$ -binding linear:8\
+#$ -l h_rt=2:00:00\
+#$ -l highp\
+#$ -N TEST\
+#$ -o test-$JOB_ID.out\
+#$ -e test-$JOB_ID.err\
+\
+##$ -l h_data=1G\
+\
+./scf C20.scf.in C.UPF\
+./xanes C20.xspectra.in}
---
+++ XANES/code/scf.sh
@@ -0,0 +1,103 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\csgray\c0;}
+\margl1440\margr1440\vieww16700\viewh8400\viewkind0
+\pard\tx560\tx1120\tx1680\tx2240\tx2800\tx3360\tx3920\tx4480\tx5040\tx5600\tx6160\tx6720\pardirnatural\partightenfactor0
+
+\f0\fs22 \cf2 \CocoaLigature0 #!/bin/sh\
+\
+start_time=`date +%s` \
+\
+cd `dirname $0`\
+SAMPLE_DIR=`pwd`\
+\
+SCF_INPUT="$1"\
+PSEUDO_INPUT="$2"\
+NAME="$\{SCF_INPUT%.scf.in\}"\
+\
+echo -e "\\n############ This script will run pw.x for $SCF_INPUT ###################\\n"\
+\
+echo -e\
+echo -e "SCF input is $SCF_INPUT"\
+echo -e "PSEUDO input is $PSEUDO_INPUT"\
+echo -e "Name of material is $NAME"\
+echo -e\
+\
+echo -e "\\n########## Loading in necessary variables and checking data ###############\\n"\
+\
+\
+echo -e "\\nLoading in environment variables..."\
+. ../../environment_variables \
+echo -e "Done.\\n"\
+\
+BIN_DIR="$SAMPLE_DIR/../../bin"\
+PSEUDO_DIR="$SAMPLE_DIR/pseudo"\
+RESULTS_DIR="$SAMPLE_DIR/results"\
+TMP_DIR="$SAMPLE_DIR/results/tmp"\
+\
+echo "BIN_DIR is set to: $BIN_DIR"\
+echo "PSEUDO_DIR is set to: $PSEUDO_DIR"\
+echo "RESULTS_DIR is set to: $RESULTS_DIR"\
+echo "TMP_DIR is set to: $TMP_DIR"\
+\
+echo -e "\\nChecking directories..."\
+for DIR in "$BIN_DIR" "$PSEUDO_DIR" ; do\
+    if test ! -d $DIR ; then\
+        echo -e\
+        echo -e "ERROR: $DIR not existent or not a directory"\
+        echo -e "Aborting"\
+        exit 1\
+    fi\
+done\
+echo -e "Done.\\n"\
+\
+echo -e "\\nChecking for pw.x..."\
+if test ! -x $BIN_DIR/pw.x ; then\
+    echo -e "ERROR: $BIN_DIR/pw.x not found or not executable"\
+    echo -e "Aborting..."\
+    exit 1\
+fi\
+echo -e "Done.\\n"\
+\
+echo -e "\\nChecking for pseudofiles..."\
+for FILE in $PSEUDO_INPUT ; do\
+    if test ! -r $PSEUDO_DIR/$FILE ; then\
+        echo -e "ERROR: $PSEUDO_DIR/$FILE not existent or not readable"\
+        echo -e "Aborting..."\
+        exit 1\
+    fi\
+done\
+echo -e "Done.\\n"\
+\
+echo -e "\\nCleaning temporary directory..."\
+rm -rf $TMP_DIR/*\
+echo -e "Done.\\n"\
+\
+\
+echo -e "\\n################### Running calculations ########################\\n"\
+\
+cd $RESULTS_DIR\
+\
+PW_COMMAND="$PARA_PREFIX $BIN_DIR/pw.x $PARA_POSTFIX"\
+\
+echo -e "\\n\\nExtracting core wavefunctions from pseudofile..."\
+$SAMPLE_DIR/../tools/upf2plotcore.sh $PSEUDO_DIR/$PSEUDO_INPUT > $NAME.wfc\
+echo -e "Done.\\n"\
+\
+echo -e "\\n\\nPerforming SCF calculations on $SCF_INPUT..."\
+$PW_COMMAND < $SCF_INPUT > $NAME.scf.out\
+check_failure $?\
+echo -e "Done.\\n"\
+\
+end_time=`date +%s`\
+runtime=$((end_time - start_time))\
+\
+hours=$((runtime / 3600))\
+minutes=$(( (runtime % 3600) / 60 ))\
+seconds=$(( (runtime % 3600) % 60 ))\
+\
+echo "Runtime: $hours:$minutes:$seconds (hh:mm:ss)"\
+\
+echo -e "\\nSCF output (scf.out) and core wavefunction (.cfw) file located in $RESULTS_DIR \\n"\
+}
