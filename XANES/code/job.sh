0809056722ad60e0c0cb2edb509211d393be298d
briananguyen <109394854+chickennuggey@users.noreply.github.com>
2025-03-21 21:30:37 -0700

    Update job.sh

--- XANES/code/job.sh
+++ XANES/code/job.sh
@@ -1,15 +1,15 @@
-#!/bin/bash -l\
-\
-#$ -cwd\
-#$ -l nodes=1\
-#$ -binding linear:8\
-#$ -l h_rt=2:00:00\
-#$ -l highp\
-#$ -N TEST\
-#$ -o test-$JOB_ID.out\
-#$ -e test-$JOB_ID.err\
-\
-##$ -l h_data=1G\
-\
+#!/bin/bash -l
+
+#$ -cwd
+#$ -l nodes=1
+#$ -binding linear:8
+#$ -l h_rt=2:00:00
+#$ -l highp
+#$ -N TEST
+#$ -o test-$JOB_ID.out
+#$ -e test-$JOB_ID.err
+
+##$ -l h_data=1G
+
 ./scf C20.scf.in C.UPF
 ./xanes C20.xspectra.in
