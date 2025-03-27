adb2cd339903297637733adeb5d4aab68a59c1f3
briananguyen <109394854+chickennuggey@users.noreply.github.com>
2025-03-27 15:36:14 -0700

    Update job.sh

--- XANES/code/job.sh
+++ XANES/code/job.sh
@@ -20,10 +20,10 @@ for n in "$nat[@]"; do
     # save name
     prefix="aC1_$n"
     # copy and modifiy aC1 scf file
-    update_input_new results/aC1.scf.in results/$prefix.scf.in nat $n
+    update_input results/aC1.scf.in nat $n results/$prefix.scf.in
     update_input results/$prefix.scf.in prefix "'$prefix'"
     # copy and modify aC1 xspectra file
-    update_input_new results/aC1.xspectra.in results/$prefix.xspectra.in prefix "'$prefix'"
+    update_input results/aC1.xspectra.in prefix "'$prefix'" results/$prefix.xspectra.in
     update_input results/$prefix.xspectra.in filecore "'$prefix.wfc'"
     # run calculations
     ./scf $prefix.scf.in C_PBE_TM_2pj.UPF
@@ -35,7 +35,7 @@ done
 xgamma=(0.4 0.8 1)
 for x in "$xgamma[@]"; do
     prefix="diamond_$x"
-    update_input_new results/diamond.xspectra.in results/$prefix.xspectra.in prefix "'$prefix'"
+    update_input results/diamond.xspectra.in prefix "'$prefix'" results/$prefix.xspectra.in
     ./xanes $prefix.xspectra.in
 done  
 
@@ -44,7 +44,7 @@ done
 xgamma=(0.4 0.8 1.5 2 3)
 for x in "$xgamma[@]"; do
     prefix="aC_$x"
-    update_input_new results/aC.xspectra.in results/$prefix.xspectra.in prefix "'$prefix'"
+    update_input_new results/aC.xspectra.in prefix "'$prefix'" results/$prefix.xspectra.in
     xanes $prefix.xspectra.in
 done 
 
@@ -52,13 +52,13 @@ done
 xgamma=(0.8 1.5 2)
 for x in "$xgamma[@]"; do
     prefix="avg_aC_$x"
-    update_input_new results/aC.scf.in results/$prefix.scf.in prefix "'$prefix'"
-    update_input_new results/aC.xspectra.in results/$prefix.xspectra.in prefix "'$prefix'"
+    update_input results/aC.scf.in prefix "'$prefix'" results/$prefix.scf.in
+    update_input results/aC.xspectra.in prefix "'$prefix'" results/$prefix.xspectra.in
     ./xanes_amorphous $prefix.scf.in C_PBE_TM_2pj.UPF $prefix.xspectra.in
 done 
 
 # average diamond 
 prefix="avg_diamond_0.8"
-update_input_new results/diamond.scf.in results/$prefix.scf.in prefix "'$prefix'"
-update_input_new results/diamond.xspectra.in results/$prefix.xspectra.in prefix "'$prefix'"
+update_input results/diamond.scf.in prefix "'$prefix'" results/$prefix.scf.in
+update_input results/diamond.xspectra.in prefix "'$prefix'" results/$prefix.xspectra.in
 ./xanes_crystal $prefix.scf.in C_PBE_TM_2pj.UPF $prefix.xspectra.in
